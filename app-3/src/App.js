6e03259f3844d824a37ecbb6624e2eb4c80623ad
Ryan Morgan <rrgy80@gmail.com>
2019-10-22 14:41:04 -0600

    re-done with hooks

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,30 +1,22 @@
-import React,{Component} from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
-export default class App extends Component{
-  constructor(){
-    super()
-    this.state={
-      list: ['Pizza', 'Burrito', 'Steak', 'Orange', 'Ice cream'],
-      filter:''
-    }
-  }
-  handleChange(filter){
-    this.setState({
-      filter: filter
-    })
-  }
-  render(){
-    let list = this.state.list.filter((element) =>{
-      return element.includes (this.state.filter)})
-      .map((element,index) => {
-        return <h2 key={index}>{element}</h2>
-      })
-    return (
-      <div className='App'>
-        <input onChange={element => this.handleChange(element.target.value)}type='text'/>
-        {list}
-      </div>
-    )
-  }
+const App = () => {
+  const [list, setList] = useState(['spaghetti', 'ice cream', 'sushi', 'bologna', 'cheese'])
+  const [filterList, setFilterList] = useState('')
+
+  let listItems = list.filter((element, index) => {
+    return element.includes(filterList)
+  }).map((element, index) => {
+    return <h2 key={index}>{element}</h2>
+  })
+
+  return (
+    <div>
+      <input onChange={e => setFilterList(e.target.value)}></input>
+      {listItems}
+    </div>
+  )
 }
+
+export default App
